b739186f759078e8a73ec676618d9210512b391e
Сергей <sergeyefimov23423@gmail.com>
2026-02-19 19:41:14 +0500

    update 1.3

--- 19-21/19_21.py
+++ 19-21/19_21.py
@@ -14,3 +14,15 @@ def f(s, m):
 print('19)', [s for s in range(1, 125) if not f(s, 1) and f(s, 2)])
 print('20)', [s for s in range(1, 125) if not f(s, 1) and f(s, 3)])
 print('21)', [s for s in range(1, 125) if not f(s, 2) and f(s, 4)])
+
+"""решение 2 номера"""
+# Все рассуждения аналогичны задаче выше
+def f(a, b, m):
+    if a + b <= 200: return m % 2 == 0
+    if m == 0: return 0
+    h = [f(a - 3, b - 4, m - 1), f (a - 8, b // 2, m - 1), f (a // 2, b - 10, m - 1)]
+    return any(h) if m % 2 != 0 else all(h)
+
+print('19)', [s for s in range(100, 500) if not f(110, s, 1) and f(110, s, 2)])
+print('20)', [s for s in range(100, 500) if not f(110, s, 1) and f(110, s, 3)])
+print('21)', [s for s in range(100, 500) if not f(110, s, 2) and f(110, s, 4)])
